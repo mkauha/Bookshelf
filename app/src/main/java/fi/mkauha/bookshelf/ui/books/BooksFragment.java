c547e2705731005927109bf848d8404758ef3957
Miko Kauhanen <miko.kauhanen@gmail.com>
2020-03-08 20:37:25 +0200

    Add LocalBroadCastReceiver to receive data from AddBookActivity.

--- app/src/main/java/fi/mkauha/bookshelf/ui/books/BooksFragment.java
+++ app/src/main/java/fi/mkauha/bookshelf/ui/books/BooksFragment.java
@@ -1,6 +1,9 @@
 package fi.mkauha.bookshelf.ui.books;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -13,6 +16,7 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -30,6 +34,7 @@ public class BooksFragment extends Fragment {
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager layoutManager;
 
+    List itemsArray;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -43,6 +48,8 @@ public class BooksFragment extends Fragment {
                 textView.setText(s);
             }
         });*/
+
+        LocalBroadcastManager.getInstance(getActivity()).registerReceiver(mMessageReceiver, new IntentFilter("AddBookActivity"));
         recyclerView = (RecyclerView) root.findViewById(R.id.books_recycler_view);
 
         // use this setting to improve performance if you know that changes
@@ -54,19 +61,13 @@ public class BooksFragment extends Fragment {
         recyclerView.setLayoutManager(layoutManager);
         setHasOptionsMenu(true);
         // this is data fro recycler view
-        List itemsArray = new ArrayList();
-        itemsArray.add(new BookItem(R.drawable.temp_cover_1, "Book Cover", "none", "none", "none"));
-        itemsArray.add(new BookItem(R.drawable.temp_cover_2, "1984", "none", "none", "none"));
-        itemsArray.add(new BookItem(R.drawable.temp_cover_3, "The Jungle Book", "none", "none", "none"));
-        itemsArray.add(new BookItem(R.drawable.temp_cover_4, "Something Nasty In The Woodshed", "none", "none", "none"));
-        itemsArray.add(new BookItem(R.drawable.temp_cover_4, "Something Nasty In The Woodshed", "none", "none", "none"));
-        itemsArray.add(new BookItem(R.drawable.temp_cover_4, "Something Nasty In The Woodshed", "none", "none", "none"));
-        itemsArray.add(new BookItem(R.drawable.temp_cover_4, "Something Nasty In The Woodshed", "none", "none", "none"));
-        itemsArray.add(new BookItem(R.drawable.temp_cover_4, "Something Nasty In The Woodshed", "none", "none", "none"));
-        itemsArray.add(new BookItem(R.drawable.temp_cover_4, "Something Nasty In The Woodshed", "none", "none", "none"));
-        itemsArray.add(new BookItem(R.drawable.temp_cover_4, "Something Nasty In The Woodshed", "none", "none", "none"));
-        itemsArray.add(new BookItem(R.drawable.temp_cover_4, "Something Nasty In The Woodshed", "none", "none", "none"));
-        itemsArray.add(new BookItem(R.drawable.temp_cover_4, "Something Nasty In The Woodshed", "none", "none", "none"));
+
+        //TODO get items from file
+        itemsArray = new ArrayList();
+        itemsArray.add(new BookItem(R.drawable.temp_cover_1, "Book Title", "John McWriter", "none", "none"));
+        itemsArray.add(new BookItem(R.drawable.temp_cover_2, "1984", "George Orwell", "none", "none"));
+        itemsArray.add(new BookItem(R.drawable.temp_cover_3, "The Jungle Book", "Rudyard Kipling", "none", "none"));
+        itemsArray.add(new BookItem(R.drawable.temp_cover_4, "Something Nasty In The Woodshed", "Kyril Bonfiglioli", "none", "none"));
 
 
         // specify an adapter (see also next example)
@@ -75,6 +76,26 @@ public class BooksFragment extends Fragment {
 
         return root;
     }
+
+    private BroadcastReceiver mMessageReceiver = new BroadcastReceiver() {
+
+        int temperature;
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Bundle bundle = intent.getExtras();
+            BookItem item = (BookItem) bundle.getSerializable("BOOK_ITEM");
+            addListItem(item);
+            recyclerView.smoothScrollToPosition(recyclerView.getAdapter().getItemCount() -1);
+        }
+    };
+
+    private void addListItem(BookItem item) {
+        //TODO add to file and read file
+        itemsArray.add(item);
+
+    }
+
     @Override
     public void onPrepareOptionsMenu(Menu menu) {
         MenuItem m1 = menu.findItem(R.id.add_book);
