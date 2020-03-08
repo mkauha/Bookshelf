a03823c8c253f22a1b8b1c481d0e4bb6e527c62b
Miko Kauhanen <miko.kauhanen@gmail.com>
2020-03-08 21:48:05 +0200

    Remove BookID and add setters and getters.

--- app/src/main/java/fi/mkauha/bookshelf/items/BookItem.java
+++ app/src/main/java/fi/mkauha/bookshelf/items/BookItem.java
@@ -4,18 +4,27 @@ import java.io.Serializable;
 
 public class BookItem implements Serializable {
     private int bookID;
+    private int imageID;
     private String title;
     private String author;
     private String genre;
     private String tag;
 
-    public BookItem(int bookID, String name, String author, String genre, String tag) {
-        this.bookID = bookID;
-        this.title = name;
+    public BookItem(int imageID, String title, String author) {
+        //this.bookID = bookID;
+        this.imageID = imageID;
+        this.title = title;
         this.author = author;
+    }
+    public BookItem(int imageID, String title, String author, String genre) {
+        this(imageID, title, author);
         this.genre = genre;
+    }
+    public BookItem(int imageID, String title, String author, String genre, String tag) {
+        this(imageID, title, author, genre);
         this.tag = tag;
     }
+
 
     public int getBookID() {
         return bookID;
@@ -23,6 +32,22 @@ public class BookItem implements Serializable {
 
     public void setBookID(int bookID) {
         this.bookID = bookID;
+    }
+
+    public int getImageID() {
+        return imageID;
+    }
+
+    public void setImageID(int imageID) {
+        this.imageID = imageID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
     }
 
     public String getName() {
