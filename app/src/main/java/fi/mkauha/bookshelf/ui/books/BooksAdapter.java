c1df43601438d814a47c479dff021f33774b1f30
Miko Kauhanen <miko.kauhanen@gmail.com>
2020-03-20 14:12:09 +0200

    Move binding to holder.

--- app/src/main/java/fi/mkauha/bookshelf/ui/books/BooksAdapter.java
+++ app/src/main/java/fi/mkauha/bookshelf/ui/books/BooksAdapter.java
@@ -1,6 +1,7 @@
 package fi.mkauha.bookshelf.ui.books;
 
 import android.content.res.Resources;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,6 +11,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.squareup.picasso.Picasso;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import fi.mkauha.bookshelf.R;
@@ -19,10 +21,35 @@ public class BooksAdapter extends RecyclerView.Adapter<BooksAdapter.MyViewHolder
 
     private List<BookItem> booksList;
 
-    // Provide a reference to the views for each data item
-    // Complex data items may need more than one view per item, and
-    // you provide access to all the views for a data item in a view holder
-    public static class MyViewHolder extends RecyclerView.ViewHolder {
+    public BooksAdapter(List<BookItem> booksList) {
+        this.booksList = booksList;
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public BooksAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        // create a new view
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_book, parent, false);
+
+        MyViewHolder viewHolder = new MyViewHolder(view);
+        return viewHolder;
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(BooksAdapter.MyViewHolder holder, int position) {
+        holder.bindBookItem(booksList.get(position));
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return booksList.size();
+    }
+
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
         // each data item is just a string in this case
 
         public TextView bookTitle;
@@ -35,51 +62,21 @@ public class BooksAdapter extends RecyclerView.Adapter<BooksAdapter.MyViewHolder
             bookAuthor = (TextView) v.findViewById(R.id.book_author);
             imageView = (ImageView) v.findViewById(R.id.book_cover);
         }
+        public void bindBookItem(BookItem bookItem) {
+            Log.d("BookAdapter", "bindBookItem");
+            //bookItem = booksList.get(position);
+            bookTitle.setText(bookItem.getName());
+            bookAuthor.setText(bookItem.getAuthor());
+
+            //TODO correct placeholder images
+            Picasso.get()
+                    .load(bookItem.getImgURL())
+                    .resize(500, 700)
+                    .centerCrop()
+                    .placeholder(R.drawable.temp_cover_1)
+                    .into(imageView);
+        }
+
     }
 
-    // Provide a suitable constructor (depends on the kind of dataset)
-    public BooksAdapter(List<BookItem> booksList) {
-        this.booksList = booksList;
-    }
-
-    public void setBooksList(List<BookItem> booksList) {
-        this.booksList = booksList;
-    }
-
-    // Create new views (invoked by the layout manager)
-    @Override
-    public BooksAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent,
-                                                        int viewType) {
-        // create a new view
-        View v = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.item_book, parent, false);
-
-        MyViewHolder vh = new MyViewHolder(v);
-        return vh;
-    }
-
-    // Replace the contents of a view (invoked by the layout manager)
-    @Override
-    public void onBindViewHolder(MyViewHolder holder, int position) {
-        // - get element from your dataset at this position
-        // - replace the contents of the view with that element
-        Resources res = holder.itemView.getContext().getResources();
-        BookItem bookItem = booksList.get(position);
-        holder.bookTitle.setText(bookItem.getName());
-        holder.bookAuthor.setText(bookItem.getAuthor());
-
-        //TODO correct placeholder images
-        Picasso.get()
-                .load(bookItem.getImgURL())
-                .resize(500, 700)
-                .centerCrop()
-                .placeholder(R.drawable.temp_cover_1)
-                .into(holder.imageView);
-    }
-
-    // Return the size of your dataset (invoked by the layout manager)
-    @Override
-    public int getItemCount() {
-        return booksList.size();
-    }
 }
