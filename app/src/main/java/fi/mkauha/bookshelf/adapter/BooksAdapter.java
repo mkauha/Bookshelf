0b580a9320498f7b19ddf0449a06006d4c7a3b63
Miko Kauhanen <miko.kauhanen@gmail.com>
2020-04-05 13:58:15 +0300

    Refactor with ViewBinding.

--- app/src/main/java/fi/mkauha/bookshelf/adapter/BooksAdapter.java
+++ app/src/main/java/fi/mkauha/bookshelf/adapter/BooksAdapter.java
@@ -18,6 +18,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import fi.mkauha.bookshelf.R;
+import fi.mkauha.bookshelf.databinding.ItemBookGridBinding;
 import fi.mkauha.bookshelf.model.BookItem;
 import fi.mkauha.bookshelf.ui.details.DetailsActivity;
 import fi.mkauha.bookshelf.viewmodel.BooksViewModel;
@@ -25,8 +26,8 @@ import fi.mkauha.bookshelf.viewmodel.BooksViewModel;
 public class BooksAdapter extends RecyclerView.Adapter<BooksAdapter.ViewHolder> {
 
     BooksViewModel booksViewModel;
+    View view;
     private List<BookItem> booksList =  new ArrayList<>();
-    private String prefsKey;
 
     public BooksAdapter(BooksViewModel booksViewModel) {
         this.booksViewModel = booksViewModel;
@@ -34,69 +35,61 @@ public class BooksAdapter extends RecyclerView.Adapter<BooksAdapter.ViewHolder> 
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-
-        View view = LayoutInflater.from(parent.getContext())
+        view = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.item_book_grid, parent, false);
-        return new ViewHolder(view);
+        ItemBookGridBinding binding = ItemBookGridBinding.bind(view);
+        return new ViewHolder(binding);
     }
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
-        Log.d("BooksAdapter", " onBindViewHolder pos:" + position);
-        BookItem currentBook = booksList.get(position);
         holder.prefsKey = booksViewModel.getCurrentKey();
+
+        BookItem currentBook = booksList.get(position);
         holder.bookID = currentBook.getBookID();
-        holder.bookTitle.setText(currentBook.getTitle());
-        //holder.bookAuthor.setText(currentBook.getAuthor());
-        //holder.bookGenre.setText(currentBook.getTitle());
         holder.bookImageURL = currentBook.getImgURL();
-        holder.bookBookMarkText.setText(currentBook.getBookmark());
+
+        holder.binding.itemViewBookTitle.setText(currentBook.getTitle());
+        holder.binding.itemViewBookBookmarkText.setText(currentBook.getBookmark());
+
         if(!currentBook.getBookmark().equals("")) {
-            holder.bookBookMarkIcon.setVisibility(View.VISIBLE);
+            holder.binding.itemViewBookBookmarkIcon.setVisibility(View.VISIBLE);
         } else {
-            holder.bookBookMarkIcon.setVisibility(View.INVISIBLE);
+            holder.binding.itemViewBookBookmarkIcon.setVisibility(View.INVISIBLE);
         }
         Picasso.get()
                 .load(currentBook.getImgURL())
                 .resize(500, 700)
                 .centerCrop()
                 .placeholder(R.drawable.book_cover_placeholder)
-                .into(holder.bookImageView);
+                .into(holder.binding.itemViewBookImage);
     }
 
-    // Return the size of your dataset (invoked by the layout manager)
     @Override
     public int getItemCount() {
         return booksList.size();
     }
 
     public void setBooksList(List<BookItem> booksList) {
-        Log.d("BookAdapter", "setBooksList " + booksList.size());
         this.booksList = booksList;
-        notifyDataSetChanged();
+        //notifyDataSetChanged();
     }
 
     static class ViewHolder extends RecyclerView.ViewHolder {
-        // each data item is just a string in this case
+
+        ItemBookGridBinding binding;
+
         String prefsKey;
         int bookID;
-        public TextView bookTitle, bookAuthor, bookGenre, bookBookMarkText;
-        public ImageView bookImageView, bookBookMarkIcon;
-        public String bookImageURL;
+        String bookImageURL;
 
+        ViewHolder(ItemBookGridBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
 
-        ViewHolder(View view) {
-            super(view);
-
-            bookTitle = (TextView) view.findViewById(R.id.itemView_book_title);
-            //bookAuthor = (TextView) view.findViewById(R.id.itemView_book_author);
-            bookImageView = (ImageView) view.findViewById(R.id.itemView_book_image);
-            bookBookMarkIcon = (ImageView) view.findViewById(R.id.itemView_book_bookmark_icon);
-            bookBookMarkText = (TextView) view.findViewById(R.id.itemView_book_bookmark_text);
-            //bookGenre  = (TextView) view.findViewById(R.id.book_genre);
-            view.setOnClickListener(v -> {
+            binding.getRoot().setOnClickListener(v -> {
                 int position = getAdapterPosition();
-                Context context = view.getContext();
+                Context context = binding.getRoot().getContext();
                 //Toast.makeText(context,"Pos: " + position,Toast.LENGTH_LONG).show();
                 Intent intent = new Intent(context, DetailsActivity.class);
                 intent.putExtra("Action", "VIEW");
