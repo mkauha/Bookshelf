dc479aa60f379e0701c5f9f0b7a5737c3f9bad7a
Miko Kauhanen <miko.kauhanen@gmail.com>
2020-04-19 17:45:36 +0300

    Implement possibility to save consortium and location during view changes.

--- app/src/main/java/fi/mkauha/bookshelf/viewmodel/LibrariesViewModel.java
+++ app/src/main/java/fi/mkauha/bookshelf/viewmodel/LibrariesViewModel.java
@@ -4,16 +4,47 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import fi.mkauha.bookshelf.models.Consortium;
+
 public class LibrariesViewModel extends ViewModel {
 
-    private MutableLiveData<String> mText;
+    private MutableLiveData<Consortium> mConsortium;
+    private MutableLiveData<Double> mLatitude;
+    private MutableLiveData<Double> mLongitude;
+
+
 
     public LibrariesViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("Nothing to see here");
+        mConsortium = new MutableLiveData<>();
+        mConsortium.setValue(new Consortium(2090,"PIKI-kirjastot"));
+
+        mLatitude = new MutableLiveData<>();
+        mLatitude.setValue(61.4977606);
+        mLongitude = new MutableLiveData<>();
+        mLongitude.setValue(23.7507924);
     }
 
-    public LiveData<String> getText() {
-        return mText;
+    public void setConsortium(Consortium consortium) {
+        mConsortium.setValue(consortium);
+    }
+
+    public void setLatitude(Double latitude) {
+        mLatitude.setValue(latitude);
+    }
+
+    public void setLongitude(Double longitude) {
+        mLongitude.setValue(longitude);
+    }
+
+    public LiveData<Consortium> getConsortium() {
+        return mConsortium;
+    }
+
+    public LiveData<Double> getLatitude() {
+        return mLatitude;
+    }
+
+    public LiveData<Double> getLongitude() {
+        return mLongitude;
     }
 }
