b6f82098032844ba5c81596af7adf5c2d9b97444
Miko Kauhanen <miko.kauhanen@gmail.com>
2020-04-19 16:06:07 +0300

    Create Consortium POJO.

---
+++ app/src/main/java/fi/mkauha/bookshelf/models/Consortium.java
@@ -0,0 +1,35 @@
+package fi.mkauha.bookshelf.models;
+
+public class Consortium {
+    private long id;
+    private String name;
+
+    public Consortium(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Consortium{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
