b4eb15d24974c9842ec4b365b6d717ad97a84b86
Miko Kauhanen <miko.kauhanen@gmail.com>
2020-06-10 14:38:50 +0300

    Create SearchFragment.

---
+++ app/src/main/java/fi/mkauha/bookshelf/ui/search/SearchFragment.java
@@ -0,0 +1,27 @@
+package fi.mkauha.bookshelf.ui.search;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import fi.mkauha.bookshelf.databinding.FragmentSearchBinding;
+
+public class SearchFragment  extends Fragment {
+    private FragmentSearchBinding binding;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        Log.d("SearchFragment", "onCreateView " + this);
+        binding = FragmentSearchBinding.inflate(inflater, container, false);
+        View view = binding.getRoot();
+        return view;
+    }
+
+}
