40f8559a453a752c41a568dacfa34a222b9f2f72
Miko Kauhanen <miko.kauhanen@gmail.com>
2020-06-11 16:09:00 +0300

    Create Bottom Navigation sheet.

--- app/src/main/java/fi/mkauha/bookshelf/ui/bottomnav/BottomNavigationFragment.java
+++ app/src/main/java/fi/mkauha/bookshelf/ui/bottomnav/BottomNavigationFragment.java
@@ -1,14 +1,18 @@
 package fi.mkauha.bookshelf.ui.bottomnav;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+import com.google.android.material.navigation.NavigationView;
 
 import fi.mkauha.bookshelf.R;
 import fi.mkauha.bookshelf.databinding.FragmentBottomsheetBinding;
@@ -19,7 +23,17 @@ public class BottomNavigationFragment extends BottomSheetDialogFragment {
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_bottomsheet, container, false);
+        binding = FragmentBottomsheetBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+        NavigationView navigationView = root.findViewById(R.id.bottom_sheet_navigation_view);
+        navigationView.setNavigationItemSelectedListener(item -> {
+            NavController navController = Navigation.findNavController(getActivity(), R.id.nav_host_fragment);
+            NavigationUI.onNavDestinationSelected(item, navController);
+            this.dismiss();
+            return false;
+        });
+
+        return root;
     }
 
 }
