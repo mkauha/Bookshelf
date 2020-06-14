374df1813ea74df38269a8520e21cdeb6097cb8b
Miko Kauhanen <miko.kauhanen@gmail.com>
2020-06-14 16:56:18 +0300

    Change to non-deprecated ViewModel implementation.

--- app/src/main/java/fi/mkauha/bookshelf/ui/books/BooksFragment.java
+++ app/src/main/java/fi/mkauha/bookshelf/ui/books/BooksFragment.java
@@ -1,6 +1,8 @@
 package fi.mkauha.bookshelf.ui.books;
 
-import android.os.Build;
+import android.content.Context;
+import android.content.SharedPreferences;
+
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -9,14 +11,17 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProviders;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.bottomappbar.BottomAppBar;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
@@ -24,8 +29,9 @@ import fi.mkauha.bookshelf.R;
 import fi.mkauha.bookshelf.adapter.BooksAdapter;
 import fi.mkauha.bookshelf.databinding.FragmentBooksBinding;
 import fi.mkauha.bookshelf.models.BookItem;
+import fi.mkauha.bookshelf.ui.dialogs.AddBookModalFragment;
+import fi.mkauha.bookshelf.util.IDGenerator;
 import fi.mkauha.bookshelf.viewmodel.BooksViewModel;
-import fi.mkauha.bookshelf.viewmodel.CustomViewModelFactory;
 
 import static fi.mkauha.bookshelf.viewmodel.BooksViewModel.MY_BOOKS_KEY;
 
@@ -44,6 +50,10 @@ public class BooksFragment extends Fragment  {
     private BooksViewModel booksViewModel;
     private BooksAdapter mAdapter;
     private List<BookItem> mModels;
+    FloatingActionButton fab;
+    BottomAppBar bottomAppBar;
+    private List<BookItem> myBooksRepository;
+    public static final String SHARED_PREFS = "bookshelf_preferences";
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -69,7 +79,23 @@ public class BooksFragment extends Fragment  {
         binding = FragmentBooksBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
         setHasOptionsMenu(true);
-        FloatingActionButton fab = getActivity().findViewById(R.id.fab);
+        fab = getActivity().findViewById(R.id.fab);
+        fab.setImageDrawable(getActivity().getDrawable(R.drawable.ic_outline_add_24));
+        fab.setOnClickListener(view -> {
+            FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            AddBookModalFragment fragment = new AddBookModalFragment();
+            fragmentTransaction.add(fragment, "BottomSheetFragment");
+            fragmentTransaction.addToBackStack(null);
+
+            fragmentTransaction.commit();
+        });
+
+        bottomAppBar = (BottomAppBar) getActivity().findViewById(R.id.bottom_app_bar);
+        bottomAppBar.setFabAlignmentMode(BottomAppBar.FAB_ALIGNMENT_MODE_CENTER);
+        bottomAppBar.replaceMenu(R.menu.bottom_main_menu);
+        bottomAppBar.setNavigationIcon(R.drawable.ic_outline_menu_24);
+        bottomAppBar.setOnClickListener(view -> {});
         binding.booksRecyclerView.setLayoutManager(new GridLayoutManager(getActivity(),3));
         binding.booksRecyclerView.setHasFixedSize(true);
         binding.booksRecyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
@@ -84,19 +110,21 @@ public class BooksFragment extends Fragment  {
             }
         });
 
-
-        booksViewModel = ViewModelProviders.of(this, new CustomViewModelFactory(getActivity().getApplication())).get(BooksViewModel.class);
+        this.myBooksRepository = new ArrayList<>();
+        booksViewModel = new ViewModelProvider(getActivity()).get(BooksViewModel.class);
+        Log.d("BooksFragment: ", "booksViewModel: " + booksViewModel);
         booksViewModel.setCurrentKey(MY_BOOKS_KEY);
+        loadDummyMyBooks();
 
         final Comparator<BookItem> alphabeticalComparator = (a, b) -> a.getTitle().compareTo(b.getTitle());
 
-        mAdapter = new BooksAdapter(getContext(), alphabeticalComparator);
+        mAdapter = new BooksAdapter(getContext(), alphabeticalComparator, booksViewModel);
         binding.booksRecyclerView.setAdapter(mAdapter);
 
-        booksViewModel.getMyBooksLiveData().observe(this,
+        booksViewModel.getMyBooksLiveData().observe(getActivity(),
             list -> {
                 mModels = list;
-                mAdapter.prefsKey = booksViewModel.getCurrentKey();
+                mAdapter.prefsKey = SHARED_PREFS;
                 mAdapter.edit()
                         .replaceAll(list)
                         .commit();
@@ -104,6 +132,21 @@ public class BooksFragment extends Fragment  {
         );
 
         return root;
+    }
+
+    public void loadDummyMyBooks() {
+        if(myBooksRepository.isEmpty()) {
+            Context context = getContext();
+            myBooksRepository.add(new BookItem(IDGenerator.generate(context), "Moby Dick", "Herman Melville", "Seikkailukirjallisuus", "https://www.nauticalmind.com/wp-content/uploads/2018/04/Moby-Dick-Illustrated.jpg"));
+            myBooksRepository.add(new BookItem(IDGenerator.generate(context), "Vuonna 1984", "George Orwell", "Dystopia", "https://s22735.pcdn.co/wp-content/uploads/1984-book-covers-2.jpg"));
+            myBooksRepository.add(new BookItem(IDGenerator.generate(context), "Täällä pohjantähden alla", "Väinö Linna", "Historiallinen romaani", "https://images.gr-assets.com/books/1245092828l/1229089.jpg"));
+            myBooksRepository.add(new BookItem(IDGenerator.generate(context), "Sinuhe egyptiläinen", "Mika Waltari", "Historiallinen romaani", "https://upload.wikimedia.org/wikipedia/fi/8/88/Sinuhe_egyptil%C3%A4inen.jpg"));
+            myBooksRepository.add(new BookItem(IDGenerator.generate(context), "Tuntematon sotilas", "Väinö Linna", "Sotakirjallisuus", "https://upload.wikimedia.org/wikipedia/fi/7/79/Tuntematon_sotilas_kansi.jpg"));
+            myBooksRepository.add(new BookItem(IDGenerator.generate(context), "Taru sormusten herrasta", "J.R.R. Tolkien", "Fantasia", "https://upload.wikimedia.org/wikipedia/fi/7/77/Taru_sormusten_herrasta.jpg"));
+            myBooksRepository.add(new BookItem(IDGenerator.generate(context), "Hohto", "Stephen King", "Kauhu", "http://profspevack.com/wp-content/uploads/2009/09/ADV2360_swilliams_book.jpg"));
+            myBooksRepository.add(new BookItem(IDGenerator.generate(context), "Kalevala", "Elias Lönnrot", "Eeppinen runous", "https://kbimages1-a.akamaihd.net/dc7bc5a1-649f-4358-b232-b38d347cabda/353/569/90/False/kalevala-22.jpg"));
+        }
+        booksViewModel.setBooks(myBooksRepository);
     }
 
 /*    @Override
