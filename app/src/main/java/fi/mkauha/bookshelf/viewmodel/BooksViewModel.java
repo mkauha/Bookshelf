6a0d11cd365b310d8885a7abe714e20f092d3dcb
Miko Kauhanen <miko.kauhanen@gmail.com>
2020-06-16 20:54:49 +0300

    Modify BooksViewModel to use AndroidX Room repository.

--- app/src/main/java/fi/mkauha/bookshelf/viewmodel/BooksViewModel.java
+++ app/src/main/java/fi/mkauha/bookshelf/viewmodel/BooksViewModel.java
@@ -1,86 +1,45 @@
 package fi.mkauha.bookshelf.viewmodel;
 
 
+import android.app.Application;
 import android.util.Log;
 
+import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
-import androidx.lifecycle.ViewModel;
 
 import java.util.List;
 
-import fi.mkauha.bookshelf.models.BookItem;
-import fi.mkauha.bookshelf.util.PreferencesUtility;
+import fi.mkauha.bookshelf.models.Book;
+import fi.mkauha.bookshelf.repository.BookRepository;
 
-public class BooksViewModel extends ViewModel {
+public class BooksViewModel extends AndroidViewModel {
 
-    public static final String MY_BOOKS_KEY = "my_books";
-    public static final String WISHLIST_BOOKS_KEY = "wishlist_books";
+    private BookRepository mBooksRepository;
+    private LiveData<List<Book>> mAllBooks;
+    private final MutableLiveData<Book> selected = new MutableLiveData<>();
 
-    private String currentKey;
-    PreferencesUtility prefsUtils;
-    private List<BookItem> myBooksRepository;
-    private MutableLiveData<List<BookItem>> myBooksLiveData;
-    private final MutableLiveData<BookItem> selected = new MutableLiveData<BookItem>();
+    public BooksViewModel(Application application) {
+        super(application);
+        this.mBooksRepository = new BookRepository(application);
+        this.mAllBooks = mBooksRepository.getAllWords();
 
-    public BooksViewModel() {
-        super();
-        currentKey = MY_BOOKS_KEY;
-        this.myBooksLiveData = new MutableLiveData<>();
     }
 
-    public void select(BookItem item) {
-        Log.d("BooksViewModel", "select: " + item);
-        selected.setValue(item);
+    public void select(Book book) {
+        Log.d("BooksViewModel", "select: " + book);
+        selected.setValue(book);
     }
 
-    public MutableLiveData<BookItem> getSelected() {
+    public MutableLiveData<Book> getSelected() {
         return selected;
     }
 
 
-    public LiveData<List<BookItem>> getMyBooksLiveData() {
-        if(myBooksLiveData == null) {
-            myBooksLiveData = new MutableLiveData<>();
-        }
-        return myBooksLiveData;
+    public LiveData<List<Book>> getAllBooks() {
+        return mAllBooks;
     }
 
-    public void setBooks(List<BookItem> myBooksRepository) {
-        myBooksLiveData.setValue(myBooksRepository);
-    }
-
-    public MutableLiveData<List<BookItem>> getBooks() {
-        return this.myBooksLiveData;
-    }
-
-    public void putOne(String key, BookItem book) {
-        prefsUtils.putOne(key, book);
-    }
-
-    public BookItem getOne(String key, int id) {
-        return prefsUtils.getOne(key, id);
-    }
-
-    public void updateOne(String key, BookItem book ) {
-        prefsUtils.updateOne(key, book);
-    }
-
-    public void removeOne(String key, int id) {
-        prefsUtils.removeOne(key, id);
-    }
-
-    public void deleteAll() {
-        myBooksRepository.clear();
-    }
-
-    public void setCurrentKey(String key) {
-        this.currentKey = key;
-    }
-
-    public String getCurrentKey() {
-        return this.currentKey;
-    }
-
+    public void insert(Book book) { mBooksRepository.insert(book); }
 
 }
