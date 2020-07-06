fd161832a543701aa8eacaf6bbd62548465db008
Miko Kauhanen <miko.kauhanen@gmail.com>
2020-07-06 16:49:17 +0300

    Remove observer from book.

--- app/src/main/java/fi/mkauha/bookshelf/ui/books/BookDetailsFragment.java
+++ app/src/main/java/fi/mkauha/bookshelf/ui/books/BookDetailsFragment.java
@@ -21,6 +21,7 @@ import com.squareup.picasso.Picasso;
 
 import fi.mkauha.bookshelf.R;
 import fi.mkauha.bookshelf.databinding.FragmentBookdetailsBinding;
+import fi.mkauha.bookshelf.models.Book;
 import fi.mkauha.bookshelf.viewmodel.BooksViewModel;
 
 public class BookDetailsFragment extends Fragment {
@@ -29,11 +30,13 @@ public class BookDetailsFragment extends Fragment {
     private BooksViewModel booksViewModel;
     private BottomAppBar bottomAppBar;
     private FloatingActionButton fab;
+    private Book _book;
 
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        Log.d("BookDetailsFragment", "onCreateView");
         super.onCreateView(inflater, container, savedInstanceState);
         binding = FragmentBookdetailsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
@@ -55,20 +58,19 @@ public class BookDetailsFragment extends Fragment {
         bottomAppBar.replaceMenu(R.menu.bottom_book_details_menu);
         booksViewModel = new ViewModelProvider(getActivity()).get(BooksViewModel.class);
 
-        booksViewModel.getSelected().observe(getActivity(), book -> {
-            binding.bookdetailsBookTitle.setText(book.getTitle());
-            binding.bookdetailsBookAuthors.setText(book.getAuthor());
-            binding.bookdetailsBookYear.setText(book.getYear());
-            binding.bookdetailsBookLanguage.setText(book.getLanguages());
-            binding.bookdetailsBookPages.setText(String.valueOf(book.getPages()));
+        this._book = booksViewModel.getSelected().getValue();
+            binding.bookdetailsBookTitle.setText(_book.getTitle());
+            binding.bookdetailsBookAuthors.setText(_book.getAuthor());
+            binding.bookdetailsBookYear.setText(_book.getYear());
+            binding.bookdetailsBookLanguage.setText(_book.getLanguages());
+            binding.bookdetailsBookPages.setText(String.valueOf(_book.getPages()));
 
             Picasso.get()
-                    .load(book.getImage())
+                    .load(_book.getImage())
                     .resize(1300, 2300)
                     .centerCrop()
                     .placeholder(R.drawable.book_cover_placeholder)
                     .into(binding.bookdetailsCoverImage);
-        });
 
         bottomAppBar.setOnMenuItemClickListener(item -> {
             switch (item.getItemId()) {
