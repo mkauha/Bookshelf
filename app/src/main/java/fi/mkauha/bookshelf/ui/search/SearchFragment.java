f518a7b0fc327d7a56a3310d0b9621c7988d3228
Miko Kauhanen <miko.kauhanen@gmail.com>
2020-07-19 14:08:19 +0300

    Create SearchFragment

--- app/src/main/java/fi/mkauha/bookshelf/ui/search/SearchFragment.java
+++ app/src/main/java/fi/mkauha/bookshelf/ui/search/SearchFragment.java
@@ -9,11 +9,21 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.bottomappbar.BottomAppBar;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import fi.mkauha.bookshelf.R;
 import fi.mkauha.bookshelf.databinding.FragmentSearchBinding;
 
 public class SearchFragment  extends Fragment {
     private FragmentSearchBinding binding;
+    BottomAppBar bottomAppBar;
+    private MaterialToolbar topAppBar;
+    FloatingActionButton fab;
 
     @Nullable
     @Override
@@ -22,6 +32,18 @@ public class SearchFragment  extends Fragment {
         super.onCreateView(inflater, container, savedInstanceState);
         binding = FragmentSearchBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
+        bottomAppBar = requireActivity().findViewById(R.id.bottom_app_bar);
+        bottomAppBar.replaceMenu(R.menu.menu_bottom_search);
+
+        fab = requireActivity().findViewById(R.id.fab);
+        fab.hide();
+
+        binding.toolbar.setNavigationOnClickListener(v -> {
+            NavController navController = Navigation.findNavController(requireActivity(), R.id.nav_host_fragment);
+            navController.navigate(R.id.navigation_books);
+        });
+
         return root;
     }
 
