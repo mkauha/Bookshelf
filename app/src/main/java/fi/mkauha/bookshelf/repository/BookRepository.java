2ec9466473244fe6ab372940d5cac0c1a6e8c4c6
Miko Kauhanen <miko.kauhanen@gmail.com>
2020-08-26 10:58:52 +0300

    Add Filter to only query books.

--- app/src/main/java/fi/mkauha/bookshelf/repository/BookRepository.java
+++ app/src/main/java/fi/mkauha/bookshelf/repository/BookRepository.java
@@ -4,7 +4,6 @@ import android.app.Application;
 import android.util.Log;
 
 import androidx.lifecycle.LiveData;
-import androidx.lifecycle.MutableLiveData;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,8 +11,8 @@ import java.util.List;
 import fi.mkauha.bookshelf.models.Book;
 import fi.mkauha.bookshelf.network.BookResponse;
 import fi.mkauha.bookshelf.network.Record;
-import fi.mkauha.bookshelf.persistence.AppDatabase;
-import fi.mkauha.bookshelf.persistence.BookDao;
+import fi.mkauha.bookshelf.local.AppDatabase;
+import fi.mkauha.bookshelf.local.BookDao;
 import fi.mkauha.bookshelf.remote.ApiService;
 import fi.mkauha.bookshelf.remote.ApiServiceClient;
 import retrofit2.Call;
@@ -26,10 +25,13 @@ public class BookRepository {
     private LiveData<List<Book>> mLocalBooks;
     private final ApiService apiService;
     private List<Book> searchResults = new ArrayList<>();
+    private final String[] FILTERS = {
+            "format:0/Book/"
+    };
     private final String[] FIELDS = {
             "cleanIsbn",
             "title",
-            //"authors",
+            "nonPresenterAuthors",
             "genres",
             "year",
             "images",
@@ -76,7 +78,7 @@ public class BookRepository {
     }
 
     public List<Book> performRemoteSearch(String query) {
-        Call<BookResponse> call = apiService.getResults(query, "Title", FIELDS);
+        Call<BookResponse> call = apiService.getResults(query, "Title", FILTERS, FIELDS);
         call.enqueue(new Callback<BookResponse>() {
             @Override
             public void onResponse(Call<BookResponse>call, Response<BookResponse> response) {
@@ -87,16 +89,46 @@ public class BookRepository {
                 Log.d(TAG, "SIZE: " + recordList.size());
 
                 for(Record record : recordList) {
+                    String imageURL = " ";
+                    String author = "-";
+                    String language = "-";
+                    String summary = "-";
+                    String genres = "-";
+                    if(!record.getImages().isEmpty()) {
+                        imageURL = "https://api.finna.fi" + record.getImages().get(0);
+                        //Log.d(TAG, "IMAGE:" + imageURL);
+                    }
+
+                    if(!record.getNonPresenterAuthors().isEmpty()) {
+                        author = record.getNonPresenterAuthors().get(0).getName();
+                        //Log.d(TAG, "AUTHOR:" + author);
+                    }
+
+                    if(!record.getLanguages().isEmpty()) {
+                        language = record.getLanguages().get(0);
+                       // Log.d(TAG, "LANG:" + language);
+                    }
+
+                    if(!record.getSummary().isEmpty()) {
+                        summary = record.getSummary().get(0);
+                        //Log.d(TAG, "SUMMARY:" + summary);
+                    }
+
+                    if(!record.getGenres().isEmpty()) {
+                        genres = record.getGenres().get(0);
+                        //Log.d(TAG, "SUMMARY:" + genres);
+                    }
+
                     searchResults.add(new Book(
                             record.getCleanIsbn(),
                             record.getTitle(),
-                            "AUTHORS",
-                            "GENRES",
+                            author,
+                            genres,
                             record.getYear(),
                             "PAGES",
-                            " ",
-                            "SUMMARY",
-                            "LANGUAGES",
+                            imageURL,
+                            summary,
+                            language,
                             "COLLECTION",
                             0
                     ));
