427688261b8bd8a1a5c7c8fde3f981a3855c6cb6
Miko Kauhanen <miko.kauhanen@gmail.com>
2020-09-07 18:30:41 +0300

    Hide top toolbar on when soft keyboard is open.

--- app/src/main/java/fi/mkauha/bookshelf/views/search/SearchFragment.java
+++ app/src/main/java/fi/mkauha/bookshelf/views/search/SearchFragment.java
@@ -1,11 +1,15 @@
 package fi.mkauha.bookshelf.views.search;
 
 import android.content.Context;
+import android.graphics.Rect;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
 import android.view.inputmethod.InputMethodManager;
 
 import androidx.annotation.NonNull;
@@ -25,7 +29,7 @@ import fi.mkauha.bookshelf.databinding.FragmentSearchBinding;
 import fi.mkauha.bookshelf.views.adapter.BookListLinearAdapter;
 import fi.mkauha.bookshelf.viewmodel.SearchViewModel;
 
-public class SearchFragment  extends Fragment {
+public class SearchFragment  extends Fragment implements OnKeyboardVisibilityListener {
     private static final String TAG = "SearchFragment";
 
 
@@ -42,6 +46,7 @@ public class SearchFragment  extends Fragment {
         super.onCreateView(inflater, container, savedInstanceState);
         binding = FragmentSearchBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+        setKeyboardVisibilityListener(this);
 
         bottomAppBar = requireActivity().findViewById(R.id.bottom_app_bar);
         bottomAppBar.replaceMenu(R.menu.menu_bottom_search);
@@ -50,6 +55,7 @@ public class SearchFragment  extends Fragment {
         fab.hide();
 
         binding.toolbar.setNavigationOnClickListener(v -> {
+            closeKeyboard();
             NavController navController = Navigation.findNavController(requireActivity(), R.id.nav_host_fragment);
             navController.navigate(R.id.navigation_books);
         });
@@ -62,6 +68,12 @@ public class SearchFragment  extends Fragment {
         mAdapter = new BookListLinearAdapter(getContext(), searchViewModel);
         binding.booksRecyclerView.setAdapter(mAdapter);
 
+        binding.searchTextfield.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean b) {
+
+            }
+        });
 
         binding.searchTextfield.setOnEditorActionListener((textView, i, keyEvent) -> {
             Log.d(TAG, "query: " + textView.getText().toString());
@@ -91,4 +103,39 @@ public class SearchFragment  extends Fragment {
         }
     }
 
+    private void setKeyboardVisibilityListener(final OnKeyboardVisibilityListener onKeyboardVisibilityListener) {
+        final View parentView = ((ViewGroup) getActivity().findViewById(android.R.id.content)).getChildAt(0);
+        parentView.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+
+            private boolean alreadyOpen;
+            private final int defaultKeyboardHeightDP = 100;
+            private final int EstimatedKeyboardDP = defaultKeyboardHeightDP + (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP ? 48 : 0);
+            private final Rect rect = new Rect();
+
+            @Override
+            public void onGlobalLayout() {
+                int estimatedKeyboardHeight = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, EstimatedKeyboardDP, parentView.getResources().getDisplayMetrics());
+                parentView.getWindowVisibleDisplayFrame(rect);
+                int heightDiff = parentView.getRootView().getHeight() - (rect.bottom - rect.top);
+                boolean isShown = heightDiff >= estimatedKeyboardHeight;
+
+                if (isShown == alreadyOpen) {
+                    Log.i("Keyboard state", "Ignoring global layout change...");
+                    return;
+                }
+                alreadyOpen = isShown;
+                onKeyboardVisibilityListener.onVisibilityChanged(isShown);
+            }
+        });
+    }
+
+
+    @Override
+    public void onVisibilityChanged(boolean visible) {
+        if(visible) {
+            binding.toolbar.setVisibility(View.INVISIBLE);
+        } else {
+            binding.toolbar.setVisibility(View.VISIBLE);
+        }
+    }
 }
