98908c4b80ea6c6ceb95b5f5868fe3c81f058497
Miko Kauhanen <miko.kauhanen@gmail.com>
2020-09-22 23:03:35 +0300

    Add method for retrieving collections from viewmodel.

--- app/src/main/java/fi/mkauha/bookshelf/viewmodel/BooksViewModel.java
+++ app/src/main/java/fi/mkauha/bookshelf/viewmodel/BooksViewModel.java
@@ -11,6 +11,7 @@ import androidx.lifecycle.MutableLiveData;
 import java.util.List;
 
 import fi.mkauha.bookshelf.data.local.model.Book;
+import fi.mkauha.bookshelf.data.local.model.Collection;
 import fi.mkauha.bookshelf.data.repository.BookRepository;
 
 public class BooksViewModel extends AndroidViewModel {
@@ -18,15 +19,24 @@ public class BooksViewModel extends AndroidViewModel {
 
     private BookRepository mBooksRepository;
     private LiveData<List<Book>> mAllBooks;
+    private LiveData<List<Collection>> mAllCollections;
 
     public BooksViewModel(Application application) {
         super(application);
         this.mBooksRepository = new BookRepository(application);
         this.mAllBooks = mBooksRepository.getLocalBooks();
+        this.mAllCollections = mBooksRepository.getLocalCollections();
     }
 
     public LiveData<List<Book>> getAllBooks() {
         return mAllBooks;
     }
 
+    public LiveData<List<Collection>> getAllCollections() {
+        return mAllCollections;
+    }
+
+    public void insertCollection(Collection collection) {
+        mBooksRepository.insertOrUpdateLocalCollection(collection);
+    }
 }
